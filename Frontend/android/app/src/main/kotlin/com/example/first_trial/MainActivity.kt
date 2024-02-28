9cb889ca2bf129a71a2bdbe3a1fc0772b5f54fec
Ahmet Memduh <memduhtutus@gmail.com>
2024-02-28 13:53:25 +0300

    flutter first codes

---
+++ Frontend/android/app/src/main/kotlin/com/example/first_trial/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.first_trial
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Frontend/lib/Pages/course_homepage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'AppBars/app_bars.dart';
+import 'package:first_trial/final_variables.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CourseHomePage extends StatefulWidget {
+  const CourseHomePage({super.key});
+
+  @override
+  State<CourseHomePage> createState() => _CourseHomePageState();
+}
+
+class _CourseHomePageState extends State<CourseHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return const Scaffold(
+      appBar: InstructorAppBar(),
+      backgroundColor: PoolColors.black,
+      body: Row(),
+    );
+  }
+}
