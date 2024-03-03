312da8037a60b28bcdbb68185b440c21da4401f3
Fırat Utku Gül <firatutkugul01@outlook.com>
2024-03-03 09:16:53 +0300

    Implemented getting student with Id and creating student

--- Backend/index.js
+++ Backend/index.js
@@ -16,5 +16,61 @@ connectToDb((err) => {
 });
 
 app.get("/student", (req,res) => {
+    let students = [];
 
+    db.collection('db')
+        .find()
+        .sort({id: 1})
+        .forEach(student => students.push(student))
+        .then(() => {
+            res.status(200).json(students);
+        })
+        .catch(() => {
+            res.status(500).json({error: "Coudlnt fetch students"});
+        })
+});
+
+app.get("/student/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    if (!isNaN(id) && Number.isInteger(id)) {
+        db.collection('db')
+            .findOne({ id: id })
+            .then(student => {
+                if (student) {
+                    res.status(200).json(student);
+                } else {
+                    res.status(404).json({ error: 'Student not found' });
+                }
+            })
+            .catch(err => {
+                res.status(500).json({error: 'Couldnt fetch one student'});
+            });
+    } else {
+        res.status(500).json({error: 'Not a valid id'});
+    }
+
+});
+
+app.post('/student', (req, res) => {
+    const student = req.body;
+
+    db.collection('db')
+        .findOne({ id: student.id })
+        .then(existingStudent => {
+            if (existingStudent) {
+                res.status(400).json({ error: "Student with the ID exists" });
+            } else {
+                db.collection('db')
+                    .insertOne(student)
+                    .then(result => {
+                        res.status(201).json(result);
+                    })
+                    .catch(error => {
+                        res.status(500).json({ error: "Could not create new student" });
+                    });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "error checking for students" });
+        });
 });
