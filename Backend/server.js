019c1ea4a50ed54fb9334808168e6bee65717abd
bilal <bilal.kar@ug.bilkent.edu.tr>
2024-03-09 21:55:13 +0300

    structural changes 1

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,17 @@
+
 const express = require("express");
-const {connectToDb, getDb} = require("./db");
-const { ObjectId } = require("mongodb");
 const cors = require("cors");
 
-const flutterurl = "http://localhost:50885";
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/havuzdb")
+const db = mongoose.connection
+db.on('error', (error) => console.log(error))
+db.once('open', () => console.log('Database Connected'))
 
 const app = express();
 app.use(express.json());
-
 app.use(cors());
+const flutterurl = "http://localhost:50885";
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", flutterurl);
@@ -17,15 +20,14 @@ app.use((req, res, next) => {
     next();
   });
 
-let db;
-connectToDb((err) => {
-    if(! err){
-        app.listen(3000, () => {
-            console.log("listen on port 3000, database connected");
-        });
-        db = getDb();
-    }
-});
+const studentsRoute = require('./routes/student')
+app.use('/student', studentsRoute) 
+
+const adminsRoute = require('./routes/admin')
+app.use('/admin', adminsRoute) 
+
+app.listen(8080, ()=> console.log('Server Started'))
+
 
 app.get("/student", (req,res) => {
     let students = [];
