765a2e0efb1709496a3a57dc17e927e87ac62519
Ahmet Memduh <memduh.tutus@ug.bilkent.edu.tr>
2024-04-07 03:19:41 +0300

    appbar left navigationbar for studenthomepage

--- Frontend/lib/Pages/Student/student_homepage.dart
+++ Frontend/lib/Pages/Student/student_homepage.dart
@@ -1,3 +1,4 @@
+import 'package:first_trial/Pages/Student/student_widgets/left_bar.dart';
 import 'package:first_trial/Pages/Widgets/AppBars/app_bars.dart';
 import 'package:flutter/material.dart';
 
@@ -12,8 +13,8 @@ class _StudentHomepageState extends State<StudentHomepage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const StudentAppBar(),
-      body: Text("oh"),
+      appBar: StudentAppBar(),
+      body: LeftBar(),
     );
   }
 }
