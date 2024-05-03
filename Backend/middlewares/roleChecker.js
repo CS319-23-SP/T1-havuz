bf5f8d43538db450c88813654d081921eb0570fa
Fırat Utku Gül <firatutkugul01@outlook.com>
2024-05-03 23:41:04 +0300

    nein dein mutter

--- Backend/middlewares/roleChecker.js
+++ Backend/middlewares/roleChecker.js
@@ -1,6 +1,8 @@
 const roleChecker = (allowedRoles) => {
     return (req, res, next) => {
-      const { role } = req;
+      const role = req.role;
+      console.log(allowedRoles);
+      console.log(role);
       if (!allowedRoles.includes(role)) {
         return res.status(401).json({ success: false, error: `Role must be one of: ${allowedRoles.join(', ')}` });
       }
--- Frontend/lib/Pages/Section/Widgets/assignment_details.dart
+++ Frontend/lib/Pages/Section/Widgets/assignment_details.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:first_trial/Objects/assignment.dart';
+import 'package:first_trial/Objects/question.dart';
 import 'package:first_trial/Pages/Widgets/AppBars/app_bars.dart';
 import 'package:first_trial/Pages/Widgets/LeftBar/left_bar.dart';
 import 'package:first_trial/token.dart';
@@ -30,6 +31,8 @@ class _Assignment_DetailsState extends State<Assignment_Details> {
       deadline: "deadline");
   String? role = "unknown";
 
+  List<Question> questions = [];
+
   @override
   void initState() {
     super.initState();
@@ -43,8 +46,49 @@ class _Assignment_DetailsState extends State<Assignment_Details> {
       return;
     } else {
       getAssignmentById();
+      fetchQuestions();
       setState(() {});
+      questions.forEach((element) {print(element.id);});
     }
+  }
+
+  Future<void> fetchQuestions() async {
+    String? token = await TokenStorage.getToken();
+      if (token == null) {
+        throw Exception('Token not found');
+      }
+
+    assignment.questions.forEach((questionID) async{
+      try {
+      final response = await http.get(
+        Uri.http('localhost:8080', '/question/$questionID'),
+        headers: {
+          'Authorization': 'Bearer $token',
+          'Content-Type': 'application/json',
+        },
+      );
+      if (response.statusCode == 200) {
+        setState(() {
+          parseQuestionsData(json.decode(response.body));
+        });
+      } else {
+        throw Exception('Failed to fetch questions data');
+      }
+    } catch (e) {
+      print('Error fetching questions: $e');
+    }
+    });
+  }
+
+  void parseQuestionsData(dynamic responseData) {
+    List<Question> parsedQuestions = [];
+    for (var questionData in responseData['questions'] as List<dynamic>) {
+      final question = Question.fromJson(questionData);
+      parsedQuestions.add(question);
+    }
+    setState(() {
+      questions = parsedQuestions;
+    });
   }
 
   Future<void> getAssignmentById() async {
